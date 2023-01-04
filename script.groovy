949cca8b0f131a981eceb09dcd47d1aeb32c933b
Damilola Akinnifesi <daleks1987@gmail.com>
2023-01-04 06:23:59 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,14 @@
+def buildApp() {
+    echo 'building the applictaion'
+}
 
+def testApp() {
+    echo 'testing the applictaion'
+}
+
+def deployApp() {
+    echo 'deploying the applictaion'
+    echo "deploying version ${params.VERSION}"
+}
+
+return this
