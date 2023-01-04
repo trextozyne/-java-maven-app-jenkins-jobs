9957f7bb1e01ce10d300a108255b69634f6d623e
Damilola Akinnifesi <daleks1987@gmail.com>
2023-01-04 06:35:41 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo 'bilding docker image...'
     withCredentials([usernamePassword(credentialsId: '	Dockerhub-repo',passwordVariable: 'PASSWD', usernameVariable: 'USER')]) {
         sh 'docker build -t trex1987/my-repo:jma-2.0 .'
-        sh 'echo #PASSWD | docker login -u USER --password-stdin'
+        sh 'echo $PASSWD | docker login -u $USER --password-stdin'
         sh 'docker push trex1987/my-repo:jma-2.0'
     }
 }
